ce156b878b86a5d38d63f16bd7851e435fe3a652
shakibapon1234-maker <shakibapon1234@gmail.com>
2026-07-01 01:59:57 -0700

    chore: add client S001 (shakib academy) to clients-metadata.js

--- js/core/clients-metadata.js
+++ js/core/clients-metadata.js
@@ -1,1 +1,14 @@
-window.WFA_AUTO_DEPLOYED_CLIENTS = [];
+﻿window.WFA_AUTO_DEPLOYED_CLIENTS = [
+    {
+        "id":  "e55d76e4-ff31-432f-833d-0dfc53001018",
+        "customerCode":  "S001",
+        "academy":  "shakib academy",
+        "package":  "basic",
+        "institutionType":  "school",
+        "licenseKey":  "",
+        "supabaseUrl":  "https://tioqnflrzdirsmysooar.supabase.co",
+        "supabaseKey":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpb3FuZmxyemRpcnNteXNvb2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4ODkxMTEsImV4cCI6MjA5ODQ2NTExMX0.THrhS2aDrvvVz5XT-PJcpYR1q6L6NciLZsVzlcWknug",
+        "createdAt":  "2026-07-01T01:59:57.307Z",
+        "notes":  "Auto-deployed via script"
+    }
+];
