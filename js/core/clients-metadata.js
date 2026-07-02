d7b92584592519302bb5c754b602cf1191f00b81
shakibapon1234-maker <shakibapon1234@gmail.com>
2026-07-02 02:17:06 -0700

    chore: add client N001 (Nasrin Academy) to clients-metadata.js

--- js/core/clients-metadata.js
+++ js/core/clients-metadata.js
@@ -22,5 +22,17 @@
         "supabaseKey":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0cm1xeHN1bWFwbmlvbHN3YmdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NjYxOTQsImV4cCI6MjA5ODU0MjE5NH0.Co3DSYA0ekjl3qcZ9tmaASvmsw6MUnPNbU1h13R50A8",
         "createdAt":  "2026-07-01T23:24:02.667Z",
         "notes":  "Auto-deployed via script"
+    },
+    {
+        "id":  "3ee9e21d-b32e-418f-8a23-f3db9b79b603",
+        "customerCode":  "N001",
+        "academy":  "Nasrin Academy",
+        "package":  "Pro",
+        "institutionType":  "college",
+        "licenseKey":  "",
+        "supabaseUrl":  "https://kjbupdptfelohljzrfyg.supabase.co",
+        "supabaseKey":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqYnVwZHB0ZmVsb2hsanpyZnlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5Nzc1MTIsImV4cCI6MjA5ODU1MzUxMn0.iX-IyG5jEVTHTxKT2WF8O7JFvN3MzRE8SJlK4pvgAoY",
+        "createdAt":  "2026-07-02T02:17:06.541Z",
+        "notes":  "Auto-deployed via script"
     }
 ];
