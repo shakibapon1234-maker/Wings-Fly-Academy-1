7bb81dab7cca18cdad6041b0570383d83194ede8
shakibapon1234-maker <shakibapon1234@gmail.com>
2026-07-01 23:24:02 -0700

    chore: add client A001 (Safa Academy) to clients-metadata.js

--- js/core/clients-metadata.js
+++ js/core/clients-metadata.js
@@ -10,5 +10,17 @@
         "supabaseKey":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpb3FuZmxyemRpcnNteXNvb2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4ODkxMTEsImV4cCI6MjA5ODQ2NTExMX0.THrhS2aDrvvVz5XT-PJcpYR1q6L6NciLZsVzlcWknug",
         "createdAt":  "2026-07-01T01:59:57.307Z",
         "notes":  "Auto-deployed via script"
+    },
+    {
+        "id":  "2f22aeed-aea7-4bb2-a40f-75f6f2c38872",
+        "customerCode":  "A001",
+        "academy":  "Safa Academy",
+        "package":  "Basi",
+        "institutionType":  "school",
+        "licenseKey":  "",
+        "supabaseUrl":  "https://ktrmqxsumapniolswbgq.supabase.co",
+        "supabaseKey":  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0cm1xeHN1bWFwbmlvbHN3YmdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NjYxOTQsImV4cCI6MjA5ODU0MjE5NH0.Co3DSYA0ekjl3qcZ9tmaASvmsw6MUnPNbU1h13R50A8",
+        "createdAt":  "2026-07-01T23:24:02.667Z",
+        "notes":  "Auto-deployed via script"
     }
 ];
